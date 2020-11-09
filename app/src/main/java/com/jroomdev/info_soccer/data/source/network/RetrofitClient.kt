41a32bb07601bc27ad26950f92eea99a2dec05c9
hun <wanted0301@gmail.com>
2020-11-09 18:28:26 +0900

    setup dagger in network

---
+++ app/src/main/java/com/jroomdev/info_soccer/data/source/network/RetrofitClient.kt
@@ -0,0 +1,8 @@
+package com.jroomdev.info_soccer.data.source.network
+
+import javax.inject.Inject
+
+class RetrofitClient @Inject constructor(
+    private val RetrofitService: RetrofitService
+) {
+}
